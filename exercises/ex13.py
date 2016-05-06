e2d9887afbf323a86dfed862d98916c16da679e3
thechutrain <chu.chuyalan@gmail.com>
2016-05-06 14:28:48 -0400

    cool! use the argument variable to call a python script and pass in multiple variables

---
+++ exercises/ex13.py
@@ -0,0 +1,9 @@
+from sys import argv # argv is an example of a module, (like a feature) that you import
+
+# argv = argument variable 
+script, first, second, third = argv # this line unpacks the argv, and assigns it to the diff variables
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third

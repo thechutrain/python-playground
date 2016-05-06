5ffb3eaf5fee02f2e4886450c484cc81e1ae10dc
thechutrain <chu.chuyalan@gmail.com>
2016-05-05 22:02:28 -0400

    first 4 exercises are going through like a breeze, looking forward to resharpening my python skillzzz by the end of this review

---
+++ exercises/ex1.py
@@ -0,0 +1,12 @@
+# practicing printing, guess this is going to be a ground up review!
+
+print "Hello World!"
+print "Hello Again"	
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing"	
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+print "My name is chewbacca!"
+#print "I am a woookie!!!"
